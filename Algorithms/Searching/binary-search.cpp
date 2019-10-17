cddd5ed2a837492c741d319bd963dbd62e7658d2
Akash Sen <53941291+2207akash@users.noreply.github.com>
2019-10-17 23:37:06 +0530

    Create binary-search.cpp

---
+++ Algorithms/Searching/binary-search.cpp
@@ -0,0 +1,17 @@
+// Parameters: (Array, Start index, End index, Elements to be searched)
+int binarySearch(int a[], int l, int r, int key) {
+    while(l <= r) {
+        int mid = l+(r-l)/2;
+        if(a[mid] == key)
+            return mid;
+        if(a[mid] < key)
+            l = mid+1;
+        else
+            r = mid-1;
+    }
+    return -1;
+}
+
+/*
+    Note: Binary search works only in sorted lists.
+*/
